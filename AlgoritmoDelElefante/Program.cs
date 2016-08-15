93e92d7645ce3b078c2ab1b557bac4bfea3da421
saul-manich <saulmamani73@gmail.com>
2016-08-14 22:09:20 -0400

    AlgoritmoElefante

---
+++ AlgoritmoDelElefante/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmoDelElefante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elefante;
+            int contador = 1;
+            Console.WriteLine("Ingrese la cantidad que decee cantar");
+            elefante = int.Parse(Console.ReadLine());
+            while(contador<=elefante){
+                Console.WriteLine(contador+" Elefante se columpiaba sobre la tela de una araña");
+                Console.WriteLine(" como beia que recistia fueron a llamar a otro elefante");
+                contador++;
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
